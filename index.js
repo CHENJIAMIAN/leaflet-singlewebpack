09b892014d6979d781bb271dafb556bfb7293934
我的名字 <571574085@qq.com>
2019-07-25 17:10:24 +0800

    2

--- index.js
+++ index.js
@@ -10,4 +10,58 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
 }).addTo(mymap);
-           
+
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06], 
+    [51.51, -0.047]
+]).addTo(mymap);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
+
+
+mymap.on('click', onMapClick);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+
+
+L.TileLayer.Kitten = L.TileLayer.extend({
+    getTileUrl: function(coords) {
+        var i = Math.ceil( Math.random() * 4 );
+        return "https://placekitten.com/256/256?image=" + i;
+    },
+    getAttribution: function() {
+        return "<a href='https://placekitten.com/attribution.html'>PlaceKitten</a>"
+    }
+});
+
+L.tileLayer.kitten = function() {
+    return new L.TileLayer.Kitten();
+}
+
+// L.tileLayer.kitten().addTo(mymap);
+
+// L.tileLayer.chinaProvider('TianDiTu.Normal.Map').addTo(map);
